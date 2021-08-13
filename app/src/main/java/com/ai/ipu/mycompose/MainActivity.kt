e66b67ec5a0cd87274ee9cb38435599148f05dea
Jenner-Zhl <guduxinlingzl@126.com>
2021-08-13 10:05:50 +0800

    first version

---
+++ app/src/main/java/com/ai/ipu/mycompose/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.ai.ipu.mycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ai.ipu.mycompose.ui.theme.MyComposeTheme
+
+class MainActivity : ComponentActivity() {
+    var countTemp = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "Zhang")) {
+    val count = remember { mutableStateOf(0)}
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Blue)
+        }
+        Divider(color = Color.Transparent, thickness = 24.dp)
+        Counter(count.value) {
+            count.value = it
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MyComposeTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+
+    Button(onClick = {updateCount(count + 1)}) {
+        Text(text = "Clicked $count times")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
+    }
+}
