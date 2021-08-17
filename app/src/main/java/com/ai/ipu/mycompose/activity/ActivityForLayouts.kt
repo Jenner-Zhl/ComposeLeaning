5c4d2bcdffd15f117ba95916af586d6bdf86ca92
Jenner-Zhl <guduxinlingzl@126.com>
2021-08-17 19:08:09 +0800

    PhotographerCard

--- app/src/main/java/com/ai/ipu/mycompose/activity/ActivityForLayouts.kt
+++ app/src/main/java/com/ai/ipu/mycompose/activity/ActivityForLayouts.kt
@@ -3,11 +3,20 @@ package com.ai.ipu.mycompose.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ai.ipu.mycompose.activity.ui.theme.MyComposeTheme
 
 class ActivityForLayouts : ComponentActivity() {
@@ -15,10 +24,7 @@ class ActivityForLayouts : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting2("Android")
-                }
+                PhotographerCard()
             }
         }
     }
@@ -29,10 +35,37 @@ fun Greeting2(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row (modifier
+        .padding(16.dp)
+        .clickable { /**/ }
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     MyComposeTheme {
-        Greeting2("Android")
+        PhotographerCard()
     }
 }
