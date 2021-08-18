c09d6d035e208a120ecc19ecda3986e3c22504f7
Jenner-Zhl <guduxinlingzl@126.com>
2021-08-18 22:24:04 +0800

    带网络图片的list

---
+++ app/src/main/java/com/ai/ipu/mycompose/activity/ActivityForList.kt
@@ -0,0 +1,67 @@
+package com.ai.ipu.mycompose.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.ai.ipu.mycompose.activity.ui.theme.MyComposeTheme
+
+class ActivityForList : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeTheme {
+                ImageList()
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageList() {
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.google.cn/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    MyComposeTheme {
+        ImageList()
+    }
+}
