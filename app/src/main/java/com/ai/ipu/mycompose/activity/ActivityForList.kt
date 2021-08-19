b58b65257b62f474b112698204aac93be09e36fa
Jenner-Zhl <guduxinlingzl@126.com>
2021-08-19 09:09:31 +0800

    按钮控制滑动，coroutine

--- app/src/main/java/com/ai/ipu/mycompose/activity/ActivityForList.kt
+++ app/src/main/java/com/ai/ipu/mycompose/activity/ActivityForList.kt
@@ -4,21 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.ai.ipu.mycompose.activity.ui.theme.MyComposeTheme
+import kotlinx.coroutines.launch
 
 class ActivityForList : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +33,32 @@ class ActivityForList : ComponentActivity() {
 
 @Composable
 fun ImageList() {
+    val listSize = 100
     // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
-    LazyColumn(state = scrollState) {
-        items(100) {
-            ImageListItem(it)
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        Row {
+           Button(onClick = {
+               coroutineScope.launch {
+                   scrollState.animateScrollToItem(0)
+               }
+           }) {
+               Text(text = "Scroll to top")
+           }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to bottom")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
         }
     }
 }
