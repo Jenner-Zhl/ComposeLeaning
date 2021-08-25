a6aa8e90a090b5dd2ec8d7a67f3c7fcfd4b550d8
Jenner-Zhl <guduxinlingzl@126.com>
2021-08-25 09:20:31 +0800

    state hoisting 输入文字添加item后可以选择图标

--- state/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ state/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -17,12 +17,7 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -114,6 +109,8 @@ private fun randomTint(): Float {
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("")}
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default)}
+    val iconVisible = text.isNotBlank()
     Column {
         Row(
             Modifier
@@ -128,13 +125,19 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
             )
             TodoEditButton(
                 onClick = {
-                    onItemComplete(TodoItem(text))
+                    onItemComplete(TodoItem(text, icon))
                     setText("")
                 },
                 text = "add",
                 enabled = text.isNotEmpty(),
                 modifier = Modifier.align(Alignment.CenterVertically)
             )
+        }
+
+        if (iconVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(8.dp))
+        } else {
+            Spacer(modifier = Modifier.padding(16.dp))
         }
     }
 }
